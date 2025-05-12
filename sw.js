eb51e73cb1747641e900b4760459a3ad0ef376df
MUKi <mukispace@gmail.com>
2025-05-12 12:12:49 +0800

    Updates

--- sw.js
+++ sw.js
@@ -1,9 +1,24 @@
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('sw.js')
-    .then(registration => {
-      console.log('Service Worker 註冊成功:', registration);
-    })
-    .catch(error => {
-      console.error('Service Worker 註冊失敗:', error);
-    });
-}
+self.addEventListener('install', event => {
+  console.log('Service Worker 安裝中...');
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  console.log('Service Worker 已啟用');
+});
+
+self.addEventListener('push', event => {
+  const data = event.data.json();
+  self.registration.showNotification(data.title, {
+    body: data.body,
+    data: data.data
+  });
+});
+
+self.addEventListener('notificationclick', event => {
+  event.notification.close();
+  const url = event.notification.data.url;
+  event.waitUntil(
+    clients.openWindow(url)
+  );
+});
