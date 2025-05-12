3782b7435d3ef8b3261c45666f2bfd019f4919e7
MUKi <mukispace@gmail.com>
2025-05-12 11:06:52 +0800

    feat: 調整 toggleMobileMenu 函數的縮排，並新增 Service Worker 註冊功能

--- src/template.js
+++ src/template.js
@@ -1,6 +1,6 @@
 function toggleMobileMenu() {
-const mobileMenu = document.getElementById('mobile-menu');
-mobileMenu.classList.toggle('hidden');
+  const mobileMenu = document.getElementById('mobile-menu');
+  mobileMenu.classList.toggle('hidden');
 }
 
 // 配置 Nunjucks 的模板路徑
@@ -10,3 +10,15 @@ nunjucks.configure('templates', { autoescape: true });
 // document.getElementById('header').innerHTML = nunjucks.render('header.html');
 // document.getElementById('footer').innerHTML = nunjucks.render('footer.html');
 document.getElementById('sidebar').innerHTML = nunjucks.render('sidebar.html');
+
+
+// 註冊 Service Worker
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('sw.js')
+    .then(registration => {
+      console.log('Service Worker 註冊成功:', registration);
+    })
+    .catch(error => {
+      console.error('Service Worker 註冊失敗:', error);
+    });
+}
